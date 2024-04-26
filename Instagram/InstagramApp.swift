93b94ccc22f97d91e94c5bbae4c3b8e523ac0157
ichichavez <isidrochavez429@gmail.com>
2024-04-26 07:07:27 -0400

    Initial Commit

---
+++ Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Isidro Chávez on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
